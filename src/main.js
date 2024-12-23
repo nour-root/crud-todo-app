5de3a33b4a15e3cff24caed7cd1058d8309f98c1
Nour Mahmoud <nour.m.mahmod@gmail.com>
2024-12-23 14:06:43 +0300

    finished basic form

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import "./style.css";
+
+const form = document.getElementById("todo-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const title = document.getElementById("title").value;
+  const priority = document.getElementById("priority").value;
+  const description = document.getElementById("description").value;
+  console.log({ title, priority, description });
+  form.reset();
+});
